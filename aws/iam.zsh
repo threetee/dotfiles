55f51053854feba0045426fc0bae6228188d7914
Eric Dennis <me@ericdennis.com>
2014-04-23 04:34:12 -0700

    Add aws directory

---
+++ aws/iam.zsh
@@ -0,0 +1,8 @@
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export AWS_IAM_HOME="/usr/local/opt/aws-iam-tools/libexec"
+export AWS_CREDENTIAL_FILE=$HOME/.aws-credentials-master
+
+export PATH=$PATH:$AWS_IAM_HOME/bin
+
+export AWS_EB_HOME="/Users/edennis/.ec2/AWS-ElasticBeanstalk-CLI-2.6.0/eb/macosx/python2.7"
+export PATH=$PATH:$AWS_EB_HOME
